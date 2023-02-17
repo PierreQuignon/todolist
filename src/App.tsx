902d8ecac535c4e16441070f9ecb3d2f56c49b71
Pierre Quignon <p.quignon.pro@gmail.com>
2023-02-17 12:00:58 +0100

    modify somethings in the app file

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,17 @@ export interface Todo {
 
 const App: any = () => {
   const [todos, setTodos] = useState<Todo[] | []>([]); //le useState revoi une valeur de type array d'objet todo ou bien un array vide
-  const [dataTodo, setDataTodo] = useState<Todo[] | []>([]);
+  const [clicked, setClicked] = useState<boolean>(false);
 
   useEffect(() => {
     fetch("./src/dataset.json")
       .then((res) => res.json()) //'res' peut être appelé comme on le souhaite
-      .then(
-        (resJson) => setDataTodo(resJson.data) // 'resJson' peut être appelé comme on le souhaite, data est target grace au console log
-      );
-  });
-
-  if (dataTodo.length === 0)
-  return <p>loading...</p>
+      .then((resJson) => {
+        if (clicked) {
+          setTodos(resJson.data);
+        } // 'resJson' peut être appelé comme on le souhaite, data est target grace au console log
+      });
+  }, [clicked]);
 
   return (
     <>
@@ -32,7 +31,7 @@ const App: any = () => {
           ? "Les todos ne sont pas chargées"
           : "Les todos sont chargées"}
       </p>
-      <button onClick={()=> setTodos(dataTodo)}>Charger des Todos</button>
+      <button onClick={() => setClicked(true)}>Charger des Todos</button>
       <TodoCounter todos={todos} />
       <TodoContainer todos={todos} />
       <TodoAdder setTodos={setTodos} />
